617b981d70eeaf31df58aa850c7bd639322b0416
kjplunkett <kplunkett@myemma.com>
2015-01-20 17:25:04 -0600

    setup basic model

---
+++ app/models/component.js
@@ -0,0 +1,25 @@
+// Backbone Model for Web Component viewer
+
+'use strict';
+
+var Backbone = require('backbone');
+
+// Define the Component model 
+var Component = Backbone.Model.extend ({
+
+	// Default attributes of each component 
+	defaults: {
+		name: '',
+		html: '',
+		css: '',
+		js: '',
+		img: '',
+		about: ''
+	},	
+
+	initialize: function () {
+		console.log("New Component model initialized");
+	}
+});
+
+module.exports = Component;
