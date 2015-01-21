95e8e9dbc053008f93717934ab63f32b9aedf033
kjplunkett <kplunkett@myemma.com>
2015-01-21 09:39:20 -0600

    setup basic collection

---
+++ app/collections/components.js
@@ -0,0 +1,18 @@
+// Backbone Collection for Web Components
+
+'use strict';
+
+var Backbone = require('backbone');
+var Components = require('models/component');
+
+// Define the Components Collection
+var Components = Backbone.Collection.extend({
+	
+	// Set the model for this Collection to the Component
+	model: Components,
+
+	initialize: function () {
+		console.log("New Components Collection initialized");
+	}
+});
+module.exports = Components;
